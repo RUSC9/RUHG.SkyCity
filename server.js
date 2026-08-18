1b681ae02b86cb08558df16e52d4c1aecdd6f363
RUSC9 <kayrray27@gmail.com>
2026-08-18 15:08:37 -0400

    Refactor server.js to streamline app initialization

--- server.js
+++ server.js
@@ -4,25 +4,3 @@ const { APP_PORT } = require("./core/config/appConfig");
 app.listen(APP_PORT, () => {
   console.log(`Sky City backend running on port ${APP_PORT}`);
 });
-
-const express = require("express");
-const app = express();
-const cityCenterRoutes = require("./districts/city-center/routes/home");
-
-app.use(express.json());
-app.use("/", cityCenterRoutes); // City Center as root
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Sky City running on port ${PORT}`);
-});
-const express = require("express");
-const app = express();
-const routeLoader = require("./loaders/routeLoader");
-
-app.use(express.json());
-
-// Load all district routes automatically
-routeLoader(app);
-
-module.exports = app;
