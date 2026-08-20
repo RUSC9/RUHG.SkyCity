6ca59c1e564c87c0c1a06958b6ccf173ae5ca9b1
RUSC9 <kayrray27@gmail.com>
2026-08-20 07:25:42 -0400

    Refactor businessController to update routes
    Removed old routes and added a new home page route for the business district.

--- skyCity/districts/business/controllers/businessController.js
+++ skyCity/districts/business/controllers/businessController.js
@@ -1,18 +1,6 @@
-const express = require("express");
-const router = express.Router();
-const CityCenterController = require("../controllers/cityCenterController");
-
-// Home page
-router.get("/", BusinessController.getHomePage);
-
-// Featured sections
-router.get("/featured/businesses", BusinessController.getFeaturedBusinesses);
-router.get("/featured/entertainment", BusinessController.getFeaturedEntertainment);
-
-// Announcements
-router.get("/announcements", BusinessController.getAnnouncements);
-
-// Registration
-router.post("/register/business", BusinessController.registerBusiness);
-
-module.exports = router;
+// districts/business/controllers/businessController.js
+exports.getDistrictHome = (req, res) => {
+  res.render("districts/business/index", {
+    title: "Sky City – Business District",
+  });
+};
