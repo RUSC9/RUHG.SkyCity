b3fab8c2908ea55de1eded8cfe66f01223e678a3
RUSC9 <kayrray27@gmail.com>
2026-08-20 10:03:49 -0400

    Delete skyCity/districts/food-drink directory

--- skyCity/districts/food-drink/routes/foodDrink.routes.js
+++
@@ -1,7 +0,0 @@
-// districts/food-drink/routes/food-drink.routes.js
-const router = require("express").Router();
-const Food-drinkController = require("../controllers/food-drinkController");
-
-router.get("/", Food-drinkController.getDistrictHome);
-
-module.exports = router;
