0ad946b412cc693522d8172cfb9d42c762a4ed1d
RUSC9 <kayrray27@gmail.com>
2026-09-06 15:50:54 -0400

    Update homeModel.js

--- districts/cityCenter/models/homeModel.js
+++ districts/cityCenter/models/homeModel.js
@@ -1,7 +1,24 @@
-// Placeholder model structure for future database integration
+// districts/cityCenter/models/homeModel.js
 
-module.exports = {
-    featuredBusinesses: [],
-    featuredEntertainment: [],
-    announcements: []
-};
+class HomeModel {
+  constructor() {
+    this.title = "City Center";
+    this.description = "Welcome to the heart of Sky City.";
+    this.features = [
+      "Business Listings",
+      "Entertainment Events",
+      "City Services",
+      "Community Social Hub"
+    ];
+  }
+
+  getHomeData() {
+    return {
+      title: this.title,
+      description: this.description,
+      features: this.features
+    };
+  }
+}
+
+module.exports = new HomeModel();
