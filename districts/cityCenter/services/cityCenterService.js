53fa7927d8a859132fb5a1352a18405c3082a439
RUSC9 <kayrray27@gmail.com>
2026-09-06 17:40:19 -0400

    Enhance CityCenterService with new features
    Added methods for managing featured businesses and entertainment, and for registering consumers and businesses.

--- districts/cityCenter/services/cityCenterService.js
+++ districts/cityCenter/services/cityCenterService.js
@@ -1,11 +1,15 @@
 // districts/cityCenter/services/cityCenterService.js
 
-const Announcement = require('../models/Announcement');
-const homeModel = require('../models/homeModel');
+const Announcement = require("../models/Announcement");
+const homeModel = require("../models/homeModel");
 
 class CityCenterService {
   constructor() {
     this.announcements = [];
+    this.featuredBusinesses = [];
+    this.featuredEntertainment = [];
+    this.consumers = [];
+    this.businesses = [];
   }
 
   getHomeData() {
@@ -22,9 +26,26 @@ class CityCenterService {
       title,
       message
     );
-
     this.announcements.push(newAnnouncement);
     return newAnnouncement;
+  }
+
+  getFeaturedBusinesses() {
+    return this.featuredBusinesses;
+  }
+
+  getFeaturedEntertainment() {
+    return this.featuredEntertainment;
+  }
+
+  registerConsumer(data) {
+    this.consumers.push(data);
+    return { success: true, consumer: data };
+  }
+
+  registerBusiness(data) {
+    this.businesses.push(data);
+    return { success: true, business: data };
   }
 }
 
