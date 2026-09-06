b8e63ff8d4a7c52ea8d7e2b9f5b8fc59f324bc7e
RUSC9 <kayrray27@gmail.com>
2026-09-06 16:10:16 -0400

    Add files via upload

---
+++ districts/cityCenter/routes/routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const cityCenterController = require('../controllers/cityCenterController');
+
+// Homepage
+router.get('/', cityCenterController.index);
+
+// Announcements
+router.get('/announcements', cityCenterController.getAnnouncements);
+router.post('/announcements', cityCenterController.addAnnouncement);
+
+module.exports = router;
+
