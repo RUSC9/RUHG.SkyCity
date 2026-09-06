5bd9285e4508eb1066de8b47a0906e7a56e3bee8
RUSC9 <kayrray27@gmail.com>
2026-09-06 15:34:38 -0400

    Delete cityCenter directory

--- cityCenter/routes/cityCenter.routes.js
+++
@@ -1,8 +0,0 @@
-// districts/cityCenter/routes/cityCenter.routes.js
-const router = require("express").Router();
-const CityCenterController = require("../controllers/cityCenterController");
-
-router.get("/", CityCenterController.getDistrictHome);
-
-module.exports = router;
-
