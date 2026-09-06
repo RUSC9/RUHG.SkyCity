7e08ce67cd5b0a2839024005bcd63e9da668d53e
RUSC9 <kayrray27@gmail.com>
2026-09-06 16:56:46 -0400

    Delete views/districts/cityCenter/controllers directory

--- views/districts/cityCenter/controllers/cityCenterControllers.js
+++
@@ -1,7 +0,0 @@
-// districts/cityCenter/controllers/cityCenterController.js
-exports.getDistrictHome = (req, res) => {
-  res.render("districts/cityCenter/index", {
-    title: "Sky City – CityCenter District",
-  });
-};
-
